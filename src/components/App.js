ad0e5720e2215ba2c23ca78efaed0b276800bfb8
rohitdarunkar <rohitdarunkarwork@gmail.com>
2026-04-01 22:09:39 +0530

    submit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,25 @@
-
 import React from "react";
-import './../styles/App.css';
+import {
+  BrowserRouter,
+  Route,
+  Switch
+} from "react-router-dom";
 
-const App = () => {
+import Home from "./Home";
+import About from "./About";
+import Navigation from "./Navigation";
+
+function App() {
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
+    <BrowserRouter>
+      <Navigation />
+
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
